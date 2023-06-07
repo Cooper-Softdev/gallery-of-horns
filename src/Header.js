42ad4f03e1ef3c259dd4ae93bc166c1a7c1cbc4a
Cooper <cooper.softdev@gmail.com>
2023-06-06 21:41:45 -0800

    img links are broken? idk wtf

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Header extends React.Component {
   render(){
-    return <h1>Cooper's Horned Beasts</h1>
+    return <h1>Cooper's Horned Beast Compendium</h1>
   }
 }
 export default Header;
---
+++ src/HornedBeast.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './HornedBeast.css';
+import Button from 'react-bootstrap/Button';
+
+class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      favorited: 0,
+    }
+  }
+
+  incrementFavorite = () => {
+        this.setState({
+      favorited: this.state.favorited + 1
+   })
+  }
+
+  render() {
+    return(
+      <>
+        <card style ={{width: '18rem'}}>
+          <h2>{this.props.title}</h2>
+          <img src={this.props.img_url} alt={this.props.alt} title={this.props.title}></img>
+          <p>{this.props.description}</p>
+          <p>😍 {this.state.favorited} many times!</p>
+          <Button variant='dark' onClick={this.incrementFavorite} > Press Me!</Button>
+        </card>
+      </>
+    )
+  }
+}
+export default HornedBeast;
