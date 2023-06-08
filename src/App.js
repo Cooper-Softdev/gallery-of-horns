39ae95541acc1a14b34b63c4018ec1f7cda8fa97
Dylan Cooper <73309872+Cooper-Softdev@users.noreply.github.com>
2023-06-07 21:24:20 -0800

    Revert "requirements met, barely"

--- src/App.js
+++ src/App.js
@@ -3,45 +3,13 @@ import React from 'react';
 import Main from './Main'
 import Header from './Header'
 import Footer from './Footer'
-import data from './data/data.json';
-import SelectedBeast from './SelectedBeast';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      showModal: false,
-      selectedBeast: {},
-    }
-  }
-
-  handleOpenModal = (name) => {
-    const beast = data.find(beast => beast.title === name)
-    this.setState({
-      showModal: true,
-      selectedBeast: beast,
-    })
-  }
-
-  handleCloseModal = () => {
-    this.setState({
-      showModal: false
-    })
-  }
   render() {
     return (
       <>
         <Header />
-        <Main 
-          handleOpenModal={this.handleOpenModal}
-          data={data}
-        />
-        <SelectedBeast
-          showModal={this.state.showModal}
-          handleCloseModal={this.handleCloseModal}
-          selectedBeast={this.state.selectedBeast}
-        />
+        <Main />
         <Footer />
       </>
     )
