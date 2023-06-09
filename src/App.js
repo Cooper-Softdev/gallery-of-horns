772aeeaaaa1502839fc66016b455d6021e0e41c7
Cooper <cooper.softdev@gmail.com>
2023-06-08 20:15:49 -0800

    ugly but works, like my manager says about me

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Header from './Header'
 import Footer from './Footer'
 import data from './data/data.json';
 import SelectedBeast from './SelectedBeast';
+import Form from 'react-bootstrap/Form';
+
 
 class App extends React.Component {
   constructor(props) {
@@ -13,6 +15,7 @@ class App extends React.Component {
     this.state = {
       showModal: false,
       selectedBeast: {},
+      sortedData: data,
     }
   }
 
@@ -29,18 +32,59 @@ class App extends React.Component {
       showModal: false
     })
   }
+
+  handleSelect = (event) => {
+    let selected = parseInt(event.target.value);
+    let newData;
+    if (selected === 0) {
+      this.setState({
+        sortedData: data
+      })
+    } else if (selected === 1) {
+      newData = data.filter(item => item.horns === 1);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 2) {
+      newData = data.filter(item => item.horns === 2);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 3) {
+      newData = data.filter(item => item.horns === 3);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 100) {
+      newData = data.filter(item => item.horns === 100);
+      this.setState({
+        sortedData: newData
+      })
+    }
+  }
+
   render() {
     return (
       <>
         <Header />
+          <form>
+            <Form.Select onChange={this.handleSelect}>
+              <option value="0">Select Horns</option>
+              <option value="0">All Horns</option>
+              <option value="1 Horn">1 Horn</option>
+              <option value="2 Horns">2 Horns</option>
+              <option value="3 Horns">3 Horns</option>
+              <option value="100 Horns">100 Horns</option>
+            </Form.Select>
+          </form>
         <Main 
           handleOpenModal={this.handleOpenModal}
-          data={data}
+          data={this.state.sortedData}
         />
         <SelectedBeast
+          selectedBeast={this.state.selectedBeast}
           showModal={this.state.showModal}
           handleCloseModal={this.handleCloseModal}
-          selectedBeast={this.state.selectedBeast}
         />
         <Footer />
       </>
