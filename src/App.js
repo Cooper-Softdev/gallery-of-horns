839029316cfbb0098a23d6826f51705ccef95c26
Cooper <cooper.softdev@gmail.com>
2023-06-08 20:28:59 -0800

    refactored, hard to read

--- src/App.js
+++ src/App.js
@@ -33,50 +33,36 @@ class App extends React.Component {
     })
   }
 
+// asked GPT4 to refactor for readability and wow, I have a lot to learn. This is so much better.
   handleSelect = (event) => {
-    let selected = parseInt(event.target.value);
+    const selected = parseInt(event.target.value);
     let newData;
+  
     if (selected === 0) {
-      this.setState({
-        sortedData: data
-      })
-    } else if (selected === 1) {
-      newData = data.filter(item => item.horns === 1);
-      this.setState({
-        sortedData: newData
-      })
-    } else if (selected === 2) {
-      newData = data.filter(item => item.horns === 2);
-      this.setState({
-        sortedData: newData
-      })
-    } else if (selected === 3) {
-      newData = data.filter(item => item.horns === 3);
-      this.setState({
-        sortedData: newData
-      })
-    } else if (selected === 100) {
-      newData = data.filter(item => item.horns === 100);
-      this.setState({
-        sortedData: newData
-      })
+      newData = data;
+    } else {
+      newData = data.filter(item => item.horns === selected);
     }
+  
+    this.setState({
+      sortedData: newData
+    });
   }
 
   render() {
     return (
       <>
         <Header />
-          <form>
-            <Form.Select onChange={this.handleSelect}>
-              <option value="0">Select Horns</option>
-              <option value="0">All Horns</option>
-              <option value="1 Horn">1 Horn</option>
-              <option value="2 Horns">2 Horns</option>
-              <option value="3 Horns">3 Horns</option>
-              <option value="100 Horns">100 Horns</option>
-            </Form.Select>
-          </form>
+        <form>
+          <Form.Select onChange={this.handleSelect}>
+            <option value="0">Select Horns</option>
+            <option value="0">All Horns</option>
+            <option value="1">1 Horn</option>
+            <option value="2">2 Horns</option>
+            <option value="3">3 Horns</option>
+            <option value="100">100 Horns</option>
+          </Form.Select>
+        </form>
         <Main 
           handleOpenModal={this.handleOpenModal}
           data={this.state.sortedData}
